925d2e1b59fcbd87803c31d0663b57930e24a975
koichiro_osaki <koichiro_osaki@DESKTOP-J2RNUA5>
2021-12-03 11:41:32 +0900

    MS追加 MySql接続追加 マイグレーション追加 Seeder追加

---
+++ src/Services/TestMS/TestMSApi/Controllers/TodoItemController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestMSApi.Infrastructure;
+using TestMSApi.Models;
+
+namespace TestMSApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodoItemController(TodoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.TodoItems.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody]TodoItem item)
+        {
+            await _context.AddAsync(item);
+            await _context.SaveChangesAsync();
+            return Ok(item.Id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromBody] int id)
+        {
+            var isExist = await _context.TodoItems.AnyAsync(a => a.Id == id);
+            if (isExist)
+            {
+                var item = await _context.TodoItems.FirstOrDefaultAsync(a => a.Id == id);
+                _context.TodoItems.Remove(item);
+                var result = await _context.SaveChangesAsync();
+                return Ok(result);
+            }
+            return Ok(0);
+        }
+
+        //[Authorize(Policy = "ApiScope")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _context.TodoItems.FirstOrDefaultAsync(a => a.Id == id);
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Services/TestMS/TestMSApi/Seed/ContextInizializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestMSApi.Infrastructure;
+using TestMSApi.Models;
+
+namespace TestMSApi.Seed
+{
+    internal class ContextInizializer
+    {
+        internal static void Seed(TodoContext todoContext)
+        {
+            if(todoContext.TodoItems.Count() < 1)
+            {
+                var todo1 = new TodoItem()
+                {
+                    Name = "test1"
+                };
+                var todo2 = new TodoItem()
+                {
+                    Name = "test2"
+                };
+                var todo3 = new TodoItem()
+                {
+                    Name = "test3"
+                };
+                todoContext.Add(todo1);
+                todoContext.Add(todo2);
+                todoContext.Add(todo3);
+                todoContext.SaveChanges();
+            }
+        }
+    }
+}
